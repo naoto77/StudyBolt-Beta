0e536bc16bd56f49427567525130e1e16b582244
naoto1990 <naoto.ohno1990@gmail.com>
2015-08-02 17:35:19 -0700

    Update 8/2

--- Models/Card.swift
+++ Models/Card.swift
@@ -1,42 +1,22 @@
-////
-////  Card.swift
-////  Flashcard
-////
-////  Created by Naoto Ohno on 7/24/15.
-////  Copyright (c) 2015 Naoto. All rights reserved.
-////
 //
+//  Card.swift
+//  Flashcard
+//
+//  Created by Naoto Ohno on 7/24/15.
+//  Copyright (c) 2015 Naoto. All rights reserved.
+//
+
 import Foundation
 import Parse
-//
+
 class Card: PFObject, PFSubclassing {
     
-//    //In this way, which class should have @IBOutlet of text field and save button?
-//    //    var titleData: String
-//    //    var termData: String
-//    //    var definitionData: String
-//    
-//    
-//    //MARK: PFSubclassing Protocol
-//    //Does this code access StudySets class on parse? If so why return value is String?
     static func parseClassName() -> String {
         return "Card"
     }
-//
-//    //What do these codes do? Accessing colums on StudySets class?
+
+    //What do these codes do? Accessing colums on StudySets class?
     @NSManaged var studySets: StudySets?
     @NSManaged var term: String?
     @NSManaged var definition: String?
-//
-//    
-//    //    //Upload deck created
-//    //    func uploadPost() {
-//    //
-//    //        //let imageData = UIImageJPEGRepresentation(image, 0.8)
-//    //        //let imageFile = PFFile(data: imageData)
-//    //        //imageFile.save()
-//    //
-//    //        //self.imageFile = imageFile
-//    //        save()
-//    //    }
 }
--- ViewControllers/FlashCardViewController.swift
+++ ViewControllers/FlashCardViewController.swift
@@ -9,46 +9,61 @@
 import Foundation
 import Parse
 import ConvenienceKit
+import Foundation
 
 class FlashCardViewController: UIViewController, UIScrollViewDelegate{
     
+    //IBOutlet for scrollView and container view(view which contains 3 views)
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var containerView: UIView!
     
-    //flash card view outlet
+    
+    //IBOutlet for 3 flashCrad views
     @IBOutlet weak var flashCardView0: FlashCardView!
     @IBOutlet weak var flashCardView1: FlashCardView!
     @IBOutlet weak var flashCardView2: FlashCardView!
     
+    
+    //Instantiate card class in Models as an array to access to Card class on Parse
     var cardsObjects = [Card]()
     
+    //Index for 3 cards?
     var cardIndex = 0
     
-    //studySet object passed from StudySetViewController
+    //Reciever of studySet object passed from StudySetViewController
     var studySet : StudySets!
+    
     
     
     override func viewDidLoad(){
         super.viewDidLoad()
         
         
-        //fetch Card class from Parse and sort it by StudySets pointer
+        //Query to fetch Card class from Parse
         var cardsQuery = PFQuery(className: Card.parseClassName())//Card.parseClassName is same as "Card"
+        
+        //Sort query by studySet pointer
         cardsQuery.whereKey("studySets", equalTo: studySet)
-        //the values are optional so unwrap it by optional binding
+        
+        //The values are optional so unwrap it by optional binding
         if let cards = cardsQuery.findObjects() as? [Card] {
             cardsObjects = cards
         }
         
+        
         //this tells self class instance is set to delegate of scrollView
         scrollView.delegate = self //should I change the name? cuz scrollView already exists in CreateSetViewController
+        
+        
+        let termString = cardsObjects[cardIndex].term
+        flashCardView1.termInFlashCard.text = termString
+        
     }
     
-    override func viewDidAppear(animated: Bool) {
+    
+    
+    override func viewWillAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        
-        let termString = cardsObjects[0].term
-        flashCardView0.termInFlashCard.text = termString
         
     }
     
@@ -58,9 +73,12 @@ class FlashCardViewController: UIViewController, UIScrollViewDelegate{
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
     //methods controlling placements of views in scroll view
     func moveToCenter() {
-        scrollView.contentOffset = CGPoint(x: scrollView.frame.size.width, y: 0)
+        //Why should I set frameSizeHeight to contentOffset's y?
+        scrollView.contentOffset = CGPoint(x: 0, y: scrollView.frame.size.height)
     }
     
     
@@ -71,18 +89,27 @@ class FlashCardViewController: UIViewController, UIScrollViewDelegate{
 extension FlashCardViewController: UIScrollViewDelegate {
     func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
         let offset = scrollView.contentOffset
+        
+        println(offset.y)
+        println(scrollView.frame.size.height)
+        
         if offset.y != scrollView.frame.size.height {
+            
             if offset.y == 0 {
                 cardIndex -= 1
             } else {
                 cardIndex += 1
             }
+            
+            //
             moveToCenter()
+            
             
             // Also needs to update text field content
             if cardIndex > 0 {
                 flashCardView0.termInFlashCard.text = cardsObjects[cardIndex - 1].term
             }
+            
             flashCardView1.termInFlashCard.text = cardsObjects[cardIndex].term
             
             if cardIndex + 1 < cardsObjects.count {
