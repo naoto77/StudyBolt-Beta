193c5412a3790bed4787fe55a145d04103549d51
naoto1990 <naoto.ohno1990@gmail.com>
2015-08-12 11:24:33 -0700

    Update 8/12

---
+++ Flashcard/StudySetsCellInExplore.swift
@@ -0,0 +1,9 @@
+//
+//  StudySetsCellInExplore.swift
+//  Flashcard
+//
+//  Created by Naoto Ohno on 8/11/15.
+//  Copyright (c) 2015 Naoto. All rights reserved.
+//
+
+import Foundation
---
+++ ViewControllers/ExploreViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ExploreViewController.swift
+//  Flashcard
+//
+//  Created by Naoto Ohno on 8/11/15.
+//  Copyright (c) 2015 Naoto. All rights reserved.
+//
+
+import Foundation
